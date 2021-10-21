224b3bc6f343827cedac19854d27d89886c92590
fouad benayad <fouad.benayad@bioreal.local>
2021-10-21 16:46:57 +0100

    Worked on  home component

--- disnep-plus-clone/src/components/Home.js
+++ disnep-plus-clone/src/components/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Home() {
     return (
-        <div>
+        <Container>
             home
-        </div>
+        </Container>
     )
 }
 
 export default Home
+
+const Container = styled.main`
+    min-height: cal(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
+    background-color: blue;
+`
