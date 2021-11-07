3f812d959d13686c63b8230e4e1102b35a5e46ea
fouad benayad <fouad.benayad@bioreal.local>
2021-11-07 10:33:53 +0100

    Added image slider

--- disnep-plus-clone/src/components/Home.js
+++ disnep-plus-clone/src/components/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import ImgSlider from './ImgSlider'
+import Viewers from './Viewers'
 
 function Home() {
     return (
         <Container>
-            home
+            <ImgSlider />
+            <Viewers />
         </Container>
     )
 }
@@ -12,7 +15,21 @@ function Home() {
 export default Home
 
 const Container = styled.main`
-    min-height: cal(100vh - 70px);
+    min-height: calc(100vh - 70px);
     padding: 0 calc(3.5vw + 5px);
-    background-color: red;
+    position: relative;
+    overflow-x: hidden;
+
+    &:before {
+        background: url("/images/home-background.png") center center / cover 
+        no-repeat fixed;
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+    }
+
 `
