945efed2b670fb98d0bb36033c659a920b9050d8
fouadsan <F.benayad95@gmail.com>
2021-11-08 17:23:32 +0100

    Finalized the app

--- disnep-plus-clone/src/firebase.js
+++ disnep-plus-clone/src/firebase.js
@@ -8,12 +8,12 @@ import { getAuth, GoogleAuthProvider  } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBIduGPbUqmsFlBnyzpmbBzwTHdPUkNypI",
-  authDomain: "react-disnep-clone.firebaseapp.com",
-  projectId: "react-disnep-clone",
-  storageBucket: "react-disnep-clone.appspot.com",
-  messagingSenderId: "236531773122",
-  appId: "1:236531773122:web:dd548d450745833f33955b"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
@@ -21,7 +21,6 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
-// const storage = firebase.storage();
 
 export { auth, provider };
 export default db;
